5663739c191dc1648bdd08d97ecbc89ebbdea1cb
ianwambaire <ian.wambaire@strathmore.edu>
2024-10-01 09:41:40 +0300

    changed the registration form

--- register.php
+++ register.php
@@ -11,11 +11,15 @@
     <script src="script.js" defer></script>
 </head>
 <body>
-    <div class="logo">
-        <h3 class="animated-logo">
-            <span>Rosewood</span> <span>Park</span>
-        </h3>
-    <h1>Register New Client</h1>
+    <div>
+        <div class="logo">
+            <h3 class="animated-logo">
+                <span>Rosewood</span> <span>Park</span>
+            </h3>
+        </div>
+        <h1>Register New Client</h1> 
+    </div>
+    <div>
     <form id="registrationForm">
         <!-- Personal Information -->
         <fieldset>
@@ -37,40 +41,10 @@
             <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required><br><br>
         </fieldset>
 
-        <!-- Address Information -->
-        <fieldset>
-            <legend>Address</legend>
-            
-            <label for="streetAddress">Street Address:</label><br>
-            <input type="text" id="streetAddress" name="streetAddress" required><br><br>
-            
-            <label for="city">City:</label><br>
-            <input type="text" id="city" name="city" required><br><br>
-            
-            <label for="state">State/Province:</label><br>
-            <input type="text" id="state" name="state" required><br><br>
-            
-            <label for="zipCode">ZIP/Postal Code:</label><br>
-            <input type="text" id="zipCode" name="zipCode" required><br><br>
-        </fieldset>
-
-        <!-- Real Estate Preferences -->
-        <fieldset>
-            <legend>Real Estate Preferences</legend>
-            
-            <label for="propertyType">Interested in Property Type:</label><br>
-            <select id="propertyType" name="propertyType">
-                <option value="residential">Residential</option>
-                <option value="commercial">Commercial</option>
-            </select><br><br>
-            
-            <label for="budget">Budget Range (in KSH):</label><br>
-            <input type="number" id="budget" name="budget" min="0" required><br><br>
-        </fieldset>
-
         <!-- Submit Button -->
         <button type="submit">Register</button>
     </form>
+    </div>
     <div class="right-section"></div>
 </body>
 </html>
