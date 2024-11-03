d37dd2ac050bb7fc6d4afa07dcfdc2754b119d97
HNduta <hope.wanjohi@strathmore.edu>
2024-11-03 15:00:23 +0300

    added consumer key and secret

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,8 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'hio24i1YVeCB7sAlEoC6iZGaQNWkjucvYWwyR5tgfJNRXI26v'; 
+  $consumerSecret = 'VgibO2MueCX7hvLRiuWI2gcfJ8i2G967FCdGNQY3csHrC7nnC19E9fYGxQ2ULfi2'; 
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
